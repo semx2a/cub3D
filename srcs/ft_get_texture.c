23028041717c543fd998a5425fc1b44bb15bb6a1
Anais BONARD <abonard@e2r8p14.clusters.42paris.fr>
2023-01-05 16:32:05 +0100

    Reduce norm error

--- srcs/ft_get_texture.c
+++ srcs/ft_get_texture.c
@@ -2,20 +2,20 @@
 
 int	get_texture(t_game *game)
 {
-	int		end;
-	int		i;
-	int		j;
+	int	end;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (i < 4)
 	{
 		if (!(game->tex.img[i] = mlx_xpm_file_to_image(game->mlix,
-			game->textpath[i], &game->tex.width[i], &game->tex.height[i])))
+				game->textpath[i], &game->tex.width[i], &game->tex.height[i])))
 			return (ft_error("Texture doesn't download"));
 		game->tex.addr[i] = (unsigned int *)mlx_get_data_addr(game->tex.img[i],
-			&game->tex.bpp, &game->tex.line_length, &game->tex.endian);
+				&game->tex.bpp, &game->tex.line_length, &game->tex.endian);
 		if (!(game->tex.tex[i] = ft_calloc(game->tex.width[i] *
-				game->tex.height[i], sizeof(unsigned int))))
+					game->tex.height[i], sizeof(unsigned int))))
 			return (ft_error("Texture alloc memory failed"));
 		j = 0;
 		end = game->tex.width[i] * game->tex.height[i];
